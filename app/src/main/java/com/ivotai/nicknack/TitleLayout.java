0fa731f10b890da840695bc77faff9953eea6ff6
ivolianer <ivolianer2014@163.com>
2018-01-22 14:52:27 +0800

    监控网络状态变化

---
+++ app/src/main/java/com/ivotai/nicknack/TitleLayout.java
@@ -0,0 +1,15 @@
+package com.ivotai.nicknack;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class TitleLayout extends LinearLayout {
+
+    public TitleLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.act_choose_photo, this);
+    }
+
+}
--- app/src/main/java/com/ivotai/nicknack/photo/ChoosePhotoAct.kt
+++ app/src/main/java/com/ivotai/nicknack/photo/ChoosePhotoAct.kt
@@ -1,19 +1,24 @@
 package com.ivotai.nicknack.photo
 
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.BitmapFactory
+import android.net.ConnectivityManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
-import android.support.v4.content.FileProvider
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.ivotai.nicknack.R
 import kotlinx.android.synthetic.main.act_choose_photo.*
 import java.io.File
+
+
 
 
 class ChoosePhotoAct : AppCompatActivity() {
@@ -22,12 +27,18 @@ class ChoosePhotoAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_choose_photo)
         btnChoosePhoto.setOnClickListener { showChoosePhotoDialog() }
+        registerNetworkChangeReceiver()
     }
 
-    val TAKE_PHOTO = 1
-    lateinit var photo:File
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(networkChangeReceiver)
+    }
+
+    private lateinit var photo: File
 
     private fun showChoosePhotoDialog() {
+
         ChoosePhotoDialog(this, R.style.Dialog).addOnClickListeners(
                 View.OnClickListener {
                     photo = File(filesDir, "output_image.jpg")
@@ -40,18 +51,18 @@ class ChoosePhotoAct : AppCompatActivity() {
 //                        e.printStackTrace()
 //                    }
 
-                     val uri =
-                            if (Build.VERSION.SDK_INT >= 24) {
-                        FileProvider.getUriForFile(this,
-                                "com.ivotai.nicknack.fileprovider", photo)
-                    }
-                    else {
-                        Uri.fromFile(photo)
-                    }
+                    val uri =
+//                            if (Build.VERSION.SDK_INT >= 24) {
+//                        FileProvider.getUriForFile(this,
+//                                "com.ivotai.nicknack.fileprovider", photo)
+//                    }
+//                    else {
+                            Uri.fromFile(photo)
+//                    }
                     // 启动相机程序
                     val intent = Intent("android.media.action.IMAGE_CAPTURE")
                     intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
-                    startActivityForResult(intent, TAKE_PHOTO)
+                    startActivityForResult(intent, 1)
                 },
                 View.OnClickListener {
                     val intent = Intent("android.intent.action.GET_CONTENT")
@@ -63,11 +74,33 @@ class ChoosePhotoAct : AppCompatActivity() {
 
     // 这个问号很关键 Intent?
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK) {
             val bitmap = BitmapFactory.decodeFile(photo.path)
             Glide.with(this).load(bitmap).into(ivPhoto)
         }
+        if (requestCode == 2 && resultCode == Activity.RESULT_OK) {
+            val uri = data!!.data
+            val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
+            Glide.with(this).load(bitmap).into(ivPhoto)
+        }
+
     }
 
+    private var networkChangeReceiver = NetworkChangeReceiver()
+
+    private fun registerNetworkChangeReceiver(){
+        IntentFilter().apply {
+            addAction("android.net.conn.CONNECTIVITY_CHANGE")
+            registerReceiver(networkChangeReceiver, this)
+        }
+    }
+
+    inner class NetworkChangeReceiver : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val connectionManager = this@ChoosePhotoAct.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val text = "network is " + if (connectionManager.activeNetworkInfo?.isAvailable == true) "available" else "unavailable"
+            Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
